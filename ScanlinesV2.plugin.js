5ed94d7c3d3edf572955847d7d141b1bba73455f
Kneesox <teh1337taco@gmail.com>
2021-02-22 21:54:02 +0000

    Added scanlines plugin
    Added systemspace scanlines plugin.

---
+++ ScanlinesV2.plugin.js
@@ -0,0 +1,89 @@
+//META{"name":"ScanlinesV2"}*//
+
+function ScanlinesV2() {
+    this.initiated = false;
+    this.oSettings = {};
+}
+
+var style = `/* Scanlines */
+
+#overlay {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+    z-index: 1000;
+    background-image: url(https://classic.systemspace.network/res/img/ui/static/overlay.png);
+    background-repeat: all;
+    background-position: 0px 0px;
+
+    animation-name: Static;
+    animation-duration: 2s;
+    animation-iteration-count: infinite;
+    animation-timing-function: steps(4);
+
+    box-shadow: inset 0px 0px 10em rgba(0,0,0,0.4);
+}
+
+@keyframes Static {
+    0% { background-position: 0px 0px; }
+    100% { background-position: 0px 4px; }
+}
+
+#overlay2 {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+    z-index: 1000;
+    background-image: url(https://classic.systemspace.network/res/img/ui/static/overlay2.png);
+    background-repeat: all;
+    background-position: 0px 0px;
+
+    animation-name: Static;
+    animation-duration: 0.8s;
+    animation-iteration-count: infinite;
+    animation-timing-function: steps(4);
+}`
+
+ScanlinesV2.prototype.getName = function () {
+    return "Systemspace Scanlines";
+};
+ScanlinesV2.prototype.getShortName = function () {
+    return "SlV2";
+};
+ScanlinesV2.prototype.getDescription = function () {
+    return "Enables Systemspace Scanlines on BetterDiscord";
+};
+ScanlinesV2.prototype.getVersion = function () {
+    return "0.5";
+};
+ScanlinesV2.prototype.getAuthor = function () {
+    return "Kneesox";
+};
+
+//ScanlinesV2.prototype.getSettingsPanel = function () {};
+
+ScanlinesV2.prototype.load = function () {
+    //PluginUtilities.checkForUpdate(this.getName(), this.getVersion(), "");
+    BdApi.injectCSS(this.getShortName(), style);
+}
+ScanlinesV2.prototype.unload = function () {
+
+};
+ScanlinesV2.prototype.start = function () {
+  const divs = $(`<div id="overlay"></div><div id="overlay2"></div>`);
+  $("body").append(divs);
+};
+ScanlinesV2.prototype.stop = function () {
+  var overlay1 = document.getElementById("overlay");
+  var overlay2 = document.getElementById("overlay2");
+  overlay1.parentNode.removeChild(overlay1);
+  overlay2.parentNode.removeChild(overlay2);
+};
+//ScanlinesV2.prototype.onMessage = function () {};
+//ScanlinesV2.prototype.onSwitch = function () {};
